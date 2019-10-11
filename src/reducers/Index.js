045fc7b15a72dd74e764b608af2ff3f2202723ea
Luis Caraballo <luisred61@gmail.com>
2019-10-10 20:35:44 -0500

    DELETE_FAVORITE action added

--- src/reducers/Index.js
+++ src/reducers/Index.js
@@ -1,10 +1,17 @@
-const reducer = (state, action) =>{
-    
-    switch(action.type){
+const reducer = (state, action) => {
+
+    switch (action.type) {
         case 'SET_FAVORITE':
-            return{
+            const item = state.myList.find(list => (list.id === action.payload.id));
+            if (item) return state;
+            return {
                 ...state,
                 myList: [...state.myList, action.payload]
+            }
+        case 'DELETE_FAVORITE':
+            return {
+                ...state,
+                myList: state.myList.filter(items => items.id !== action.payload)
             }
         default:
             return state;
