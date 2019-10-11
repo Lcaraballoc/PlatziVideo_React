41ec3b7b857f9ef52cfe07356eb26b40c97165cc
Luis Caraballo <luisred61@gmail.com>
2019-10-11 09:42:54 -0500

    User menu changes when you login and logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { logoutRequest } from '../actions'
 import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
@@ -9,6 +10,9 @@ import userIcon from '../assets/static/user-icon.png'
 const Header = props => {
     const { user } = props;
     const hasUser = Object.keys(user).length > 0;
+    const handleLogout = () => {
+        props.logoutRequest({});
+    }
     return (
         <header className="header">
             <Link to="/">
@@ -23,12 +27,19 @@ const Header = props => {
                     <p>Perfil</p>
                 </div>
                 <ul>
-                    <li><a href="/">Cuenta</a></li>
-                    <li>
-                        <Link to="/login">
-                            Iniciar Sesion
-                    </Link>
-                    </li>
+                    {hasUser ?
+                        <li><a href="/">Cuenta</a></li>
+                        : null
+                    }
+                    {hasUser ?
+                        <li><a href="#logout" onClick={handleLogout}>Cerrar Sesion</a></li>
+                        :
+                        <li>
+                            <Link to="/login">
+                                Iniciar Sesion
+                        </Link>
+                        </li>
+                    }
                 </ul>
             </div>
         </header>
@@ -41,4 +52,8 @@ const mapStateToProps = state => {
     };
 };
 
-export default connect(mapStateToProps, null)(Header);
+const mapDispatchToProps = {
+    logoutRequest,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
